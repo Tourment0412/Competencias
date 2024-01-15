bd62dcf979a5445dffe2b37bde60869c717db5bb
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-01-15 16:00:42 -0500

    Last commit competences

---
+++ EjerciciosUhunt/LoveCalculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool compareStrings(std::string a, std::string b) {
+    return (b + a) < (a + b);
+}
+
+std::string formarNumeroMasGrande(std::vector<std::string>& subcadenas) {
+    // Ordenar las subcadenas de manera personalizada
+    std::sort(subcadenas.begin(), subcadenas.end(), compareStrings);
+
+    // Concatenar las subcadenas ordenadas para formar el número más grande
+    std::string resultado;
+    for (const auto& subcadena : subcadenas) {
+        resultado += subcadena;
+    }
+
+    return resultado;
+}
+
+int main() {
+    std::vector<std::string> subcadenas = {"1", "4", "12415"};
+    std::string resultado = formarNumeroMasGrande(subcadenas);
+    std::cout << resultado << std::endl;
+
+    return 0;
+}
